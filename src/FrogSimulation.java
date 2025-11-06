70a0ad07a2c18cc2db9b9cd23a592fa53539102e
Intersected2 <brianx8@nycstudents.net>
2025-11-06 15:34:05 -0500

    yes

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -1,13 +1,18 @@
 public class FrogSimulation {
     private int goalDistance;
     private int maxHops;
+    public FrogSimulation (){
+    }
     public FrogSimulation (int dist, int numHops){
         goalDistance = dist;
         maxHops = numHops;
     }
     public int hopDistance(){
         int x = 0;
-        x = Math.random() * Integer.MAX_VALUE * 2 - Integer.MAX_VALUE;
+        x = (int) (Math.random() * Integer.MAX_VALUE * 2 - Integer.MAX_VALUE);
         return x;
+    }
+    public boolean simulate (){
+        for (int i = 0; )
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String []args){
-        System.out.println(FrogSimulation.hopDistance);
+        FrogSimulation frogsim = new FrogSimulation();
+        System.out.println(frogsim.hopDistance());
     }
 }
