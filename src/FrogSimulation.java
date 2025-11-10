9e9ab54f1a218f4f600f685d232e5ba766995be3
Intersected2 <brianx8@nycstudents.net>
2025-11-10 15:28:58 -0500

    yes

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -9,10 +9,34 @@ public class FrogSimulation {
     }
     public int hopDistance(){
         int x = 0;
-        x = (int) (Math.random() * Integer.MAX_VALUE * 2 - Integer.MAX_VALUE);
+        x = (int) (Math.random() * 21 - 5);
+//        x = (int) (Math.random() * Integer.MAX_VALUE - (Integer.MAX_VALUE/2));
         return x;
     }
     public boolean simulate (){
-        for (int i = 0; )
+        int dist = 0;
+        int hops = 0;
+        while (dist < goalDistance){
+            hops++;
+            dist = dist + hopDistance();
+        }
+        if (hops < maxHops){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public double runSimulations (int num){
+        double score = 0;
+        double score1 = 0;
+        double score2 = 0;
+        for (int i = 0; i < num; i++){
+            score2++;
+            if (simulate() == true){
+                score1++;
+            }
+        }
+        score = score1 / score2;
+        return score;
     }
 }
